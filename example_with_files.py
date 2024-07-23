563033948fc010ef17a03e1b3c26fe69bf07896e
tcsenpai <dev@tcsenpai.com>
2024-07-23 02:31:24 +0200

    corrected lib name

--- example_with_files.py
+++ example_with_files.py
@@ -1,11 +1,11 @@
-import qraro
+import qrare
 
 with open("pika.webp", "rb") as f:
     data = f.read()
 
-qraro.bin_to_qr(data, chunk_size=1000, box_size=10, border=4)
+qrare.bin_to_qr(data, chunk_size=1000, box_size=10, border=4)
 
-test_file = qraro.qr_to_bin()
+test_file = qrare.qr_to_bin()
 
 with open("pika_out.webp", "wb") as f:
     f.write(test_file)
--- example_with_string.py
+++ example_with_string.py
@@ -1,9 +1,9 @@
-import qraro
+import qrare
 
 # Example usage
 original_data = b"This is a binary string \x00\x01\x02 with some non-printable characters."
-qraro.bin_to_qr(original_data, chunk_size=50)
+qrare.bin_to_qr(original_data, chunk_size=50)
 
-decoded_data = qraro.qr_to_bin()
+decoded_data = qrare.qr_to_bin()
 print("Decoded data:", decoded_data)
 print("Original and decoded data match:", original_data == decoded_data)
